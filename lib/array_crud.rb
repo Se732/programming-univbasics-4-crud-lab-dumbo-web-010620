94a85f991d01202a4da52624fdb85c42bfb44b38
wesley perez <wesley.m.perez@gmail.com>
2019-12-17 07:38:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-
+nba_teams = ["thunder","warriors","rockets"]
+nba_teams.shift("celtics")
 end
 
 def remove_element_from_end_of_array(array)
