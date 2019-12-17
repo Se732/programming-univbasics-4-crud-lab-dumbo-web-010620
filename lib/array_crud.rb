64886a5618274303bef27bbcf71065355fa94275
wesley perez <wesley.m.perez@gmail.com>
2019-12-17 08:16:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,27 +17,27 @@ nba_teams.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-luxury_cars = ["BMW m4","Lexus LFA","Audi RS5","Nissan GTR","Toyota camry"]
+luxury_cars = ["BMW m4","Lexus LFA","Audi RS5","Nissan GTR","arrays!"]
 luxury_cars.pop
 end
 
 def remove_element_from_start_of_array(array)
-stocks = ["NKE","GOOG","AMZN","MSFT"]
+stocks = ["wow","GOOG","AMZN","MSFT"]
 stocks.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-action_movies = ["John Wick","Avengers","Matrix","Robocop"]
+action_movies = ["John Wick","Avengers","am","Robocop"]
 action_movies[2]
 end
 
 def retrieve_first_element_from_array(array)
-trending_apps = ["Reddit","Audible","Candy crush"]
+trending_apps = ["wow","Audible","Candy crush"]
 trending_apps[0]
 end
 
 def retrieve_last_element_from_array(array)
-clothing = ["tommy hilfiger""GUESS","Lacoste"]
+clothing = ["tommy hilfiger""GUESS","arrays!"]
 clothing[-1]
 end
 
