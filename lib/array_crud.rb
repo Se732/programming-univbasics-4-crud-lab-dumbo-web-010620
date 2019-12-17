cfa41e36c0fceeb2757463b641877434e5edffcb
wesley perez <wesley.m.perez@gmail.com>
2019-12-17 07:34:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   games = ["mortal kombat","call of duty"]
-  games.push("super samsh bros ultimate")
+  games.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
